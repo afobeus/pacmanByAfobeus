042201971d3f3a9c723eb48f6c41945b4a3f1f62
Artem <artem.dzichkanets@mail.ru>
2023-01-24 17:43:58 +0300

    setting screen size correction

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 def init_pygame():
     pygame.init()
-    screen = pygame.display.set_mode(1200, 700)
+    screen = pygame.display.set_mode((1200, 700))
     pygame.display.set_caption("Packman")
 
     return screen
